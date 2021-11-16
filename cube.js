d84f4cd700859ec79f2597eb04c080dd459f05d3
1e1001 <michael@malinov.com>
2021-11-16 09:47:56 -0800

    fix rotation not being enough
    went the wrong way

--- cube.js
+++ cube.js
@@ -57,7 +57,7 @@
 		}
 		if (first) {
 			first = false;
-			cube.style.transform = "rotateY(calc(6 * var(--x))) rotateX(calc(-6 * var(--y))) rotateX(-35.264389682754deg) rotateY(45deg)";
+			cube.style.transform = "rotateY(calc(25 * var(--x))) rotateX(calc(-25 * var(--y))) rotateX(-35.264389682754deg) rotateY(45deg)";
 		}
 		if (animate)
 			currentX = lc(currentX, targetX, dt),
