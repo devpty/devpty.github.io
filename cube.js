afd8b228bb4634ef0ccfe5bc0654aa22ef62500c
1e1001 <michael@malinov.com>
2021-11-16 09:45:45 -0800

    use a normal mean instead
    oops geometric doesn't actually work well

--- cube.js
+++ cube.js
@@ -27,7 +27,7 @@
 	function windowUpdate() {
 		winW = window.innerWidth;
 		winH = window.innerHeight;
-		winS = Math.sqrt(winW * winH);
+		winS = winW + winH;
 	}
 	window.addEventListener("resize", windowUpdate);
 	windowUpdate();
@@ -57,7 +57,7 @@
 		}
 		if (first) {
 			first = false;
-			cube.style.transform = "rotateY(calc(15 * var(--x))) rotateX(calc(-15 * var(--y))) rotateX(-35.264389682754deg) rotateY(45deg)";
+			cube.style.transform = "rotateY(calc(6 * var(--x))) rotateX(calc(-6 * var(--y))) rotateX(-35.264389682754deg) rotateY(45deg)";
 		}
 		if (animate)
 			currentX = lc(currentX, targetX, dt),
